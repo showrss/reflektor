4825acefea18e712d6790c6d2eb66fcee741d81d
showRSS <showrss@karmorra.info>
2013-09-29 16:59:02 +0200

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-reflektor
-=========
+reflektor is a cache of caches. It contacts a set of given caches until it finds one that contains the requested info_hash, and then caches the request locally before serving it.
+
+Set up your own
+--
+
+reflektor requires:
+
+-   PHP 5.4
+-   nginx
+-   linux
+
+
+Configuration
+--
+    
+    server {
+        listen 80;
+    
+        root /var/www/reflektor/public/;
+        index index.html;
+    
+        server_name reflektor.karmorra.info;
+    
+        rewrite ^/torrent/(.+)\.torrent$ /serve.php?ih=$1 last;
+    
+    	location /torrents/ {
+    		internal;
+    		alias /var/www/reflektor/cache/;
+    	}
+    
+        location ~ \.php$ {
+            fastcgi_pass unix:/var/run/php5-fpm.sock;
+            fastcgi_index index.php;
+            include fastcgi_params;
+        }
+    }
+    
+Dotdeb.org sources are recommended for an easy and painless setup.
+
+Make sure permissions are correct, and that both PHP and nginx are allowed to read and write in the cache folder.
---
+++ public/serve.php
@@ -0,0 +1,81 @@
+<?php
+/* reflektor: torrent cache */
+
+$GLOBALS['CACHES'] = array( 'torrage.com' 	=>	'http://torrage.com/torrent/{info_hash}.torrent',
+							'zoink.it' 		=>	'http://zoink.it/torrent/{info_hash}.torrent',
+							'torcache.net' 	=> 	'http://torcache.net/torrent/{info_hash}.torrent',
+							'torra.ws' 		=>	'http://torra.ws/torrent/{info_hash}.torrent'	);
+
+
+$GLOBALS['LOCATION'] = dirname(__FILE__).'/../cache/';
+
+if($_GET['ih']) {
+	$ih = trim(strtoupper($_GET['ih']));
+	$ih = preg_replace('/[^A-Z0-9]/', '', $ih);
+
+	if(strlen($ih) == 40) {
+		$loc = $GLOBALS['LOCATION'].$ih.'.torrent';
+		$exists = (file_exists($loc))?true:false;
+		$notnull = (filesize($loc)>0)?true:false;
+
+		if($exists) {
+			$diff = time()-filemtime($loc);
+			$mature = ($diff>3600)?true:false;
+		} else {
+			$mature = false;
+			$diff = 0;
+		}
+
+		if($exists and $notnull) {
+			if($diff > 86400) {
+				touch($loc);
+			}
+
+			header('Content-type: application/x-bittorrent');
+			header('X-Accel-Redirect: /torrents/'.$ih.'.torrent');
+			header('Content-Disposition: attachment; filename="'.$ih.'.torrent"');
+		} elseif($exists and !$notnull and !$mature) {
+			// error: 
+			echo '<h1>Torrent not found</h1><p>Torrent not found in any of the providers. Retrying later.<br /><small>Cached miss.</small></p>';
+		} else {
+			// not exists, null or not, mature
+			touch($loc);
+			$hit = false;
+
+			foreach($GLOBALS['CACHES'] as $provider) {
+				$location = str_replace('{info_hash}', $ih, $provider);
+
+				$ch = curl_init();
+				curl_setopt($ch, CURLOPT_URL, $location);
+				curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+				curl_setopt($ch, CURLOPT_ENCODING, 'gzip,deflate');
+				curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 15);
+				curl_setopt($ch, CURLOPT_BUFFERSIZE, 512);
+				curl_setopt($ch, CURLOPT_NOPROGRESS, false);
+				curl_setopt($ch, CURLOPT_FAILONERROR, true);
+				curl_setopt($ch, CURLOPT_PROGRESSFUNCTION, function($dls, $dl, $uls, $ul){
+						return ($dl > (2048 * 1024)) ? 1 : 0;
+				});
+				$data = curl_exec($ch);
+				curl_close($ch);
+
+				if(stristr($data, '</html>')) $data = false;
+
+				if($data) break;
+			}
+
+			if($data) {
+				file_put_contents($loc, $data);
+				touch($loc);
+				
+				header('Content-type: application/x-bittorrent');
+				header('X-Accel-Redirect: /torrents/'.$ih.'.torrent');
+				header('Content-Disposition: attachment; filename="'.$ih.'.torrent"');
+			} else {
+				echo '<h1>Torrent not found</h1><p>Torrent not found in any of the providers. Retrying later.<br /><small>Try and miss.</small></p>';
+			}
+		}
+	} else {
+		echo '<h1>Invalid info_hash</h1><p>Please verify it and try again.</p>';
+	}
+}
